e3b4b6353d20e2114d3558faf3d84b97a7a70af7
Davy ROBERT <100140271+DavyR01@users.noreply.github.com>
2024-04-27 17:34:06 +0200

    save

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,33 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import dataSource from "./database/dbCheckpoint";
 import { CountryResolver } from "./resolvers/Country";
+import { Country } from "./entities/country";
 
 const start = async () => {
-  await dataSource.initialize();
+   await dataSource.initialize();
 
-  const schema = await buildSchema({
-    resolvers: [CountryResolver],
-  });
+   const countries = await Country.find();
+   if (countries.length === 0) {
+      const country = new Country;
+      country.code = "FR";
+      country.emoji = 'flagFr.png';
+      country.name = "France"
+   }
 
-  const server = new ApolloServer({
-    schema,
-  });
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
+   const schema = await buildSchema({
+      resolvers: [CountryResolver],
+   });
 
-  console.log(`🚀  Server ready at: ${url}`);
+   const server = new ApolloServer({
+      schema,
+   });
+
+   const { url } = await startStandaloneServer(server, {
+      listen: { port: 4000 },
+   });
+
+   console.log(`🚀  Server ready at: ${url}`);
 };
 
 start();
