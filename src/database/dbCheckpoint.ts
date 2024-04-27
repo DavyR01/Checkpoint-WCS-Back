7098a6010076c7280b41948fc179beca2a5f59be
Davy ROBERT <davyrobert90@gmail.com>
2024-04-27 18:09:00 +0200

    change dbCheckpoint

--- src/database/dbCheckpoint.ts
+++ src/database/dbCheckpoint.ts
@@ -1,13 +1,16 @@
+import { Country } from "src/entities/country";
 import { DataSource } from "typeorm";
 
 const dataSource = new DataSource({
-  type: "postgres",
-  host: "db",
-  port: 5432,
-  username: "postgres",
-  password: "checkpoint",
-  database: "postgres",
-  entities: [`${__dirname}/../entities/*{.js,.ts}`],
+  type: "sqlite",
+  // type: "postgres",
+  // host: "db",
+  // port: 5432,
+  // username: "postgres",
+  // password: "checkpoint",
+  database: "../db_checkpoint.sqlite",
+  // entities: ["src/entities/*.ts"],
+  entities: [Country],
   synchronize: true,
 //   logging: ["query", "error"],
 });
--- src/resolvers/Country.ts
+++ src/resolvers/Country.ts
@@ -4,8 +4,7 @@ import { Query, Resolver } from "type-graphql";
 @Resolver()
 export class CountryResolver {
    @Query(() => [Country])
-   async getAllAds() {
-      return await Country.find({
-      });
+   async getAllCountries() {
+      return await Country.find();
    }
 }
