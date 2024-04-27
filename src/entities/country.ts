b14bf84cef0e6396c2f389f7d4383f7891a7bf9a
Davy ROBERT <100140271+DavyR01@users.noreply.github.com>
2024-04-27 17:22:16 +0200

    save

---
+++ src/entities/country.ts
@@ -0,0 +1,28 @@
+import {
+   BaseEntity,
+   Column,
+   Entity,
+   PrimaryGeneratedColumn,
+ } from "typeorm";
+ import { Field, ObjectType } from "type-graphql";
+ 
+ @ObjectType()
+ @Entity()
+ export class Country extends BaseEntity {
+   @Field()
+   @PrimaryGeneratedColumn()
+   id: number;
+ 
+   @Field()
+   @Column()
+   code: string;
+ 
+   @Field()
+   @Column()
+   name: string;
+
+   @Field()
+   @Column()
+   emoji: string; 
+ }
+ 
