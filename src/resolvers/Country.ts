4c545f79c5fbe978d323f9a616eb27c571dbb8b3
Davy ROBERT <davyrobert90@gmail.com>
2024-04-28 15:15:40 +0200

    add method "findOne" from typeORM for one country by code

--- src/resolvers/Country.ts
+++ src/resolvers/Country.ts
@@ -1,10 +1,19 @@
 import { Country } from "../entities/country";
-import { Query, Resolver } from "type-graphql";
+import { Arg, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class CountryResolver {
    @Query(() => [Country])
    async getAllCountries() {
       return await Country.find();
+   }
+
+   @Query(() => Country)
+   async getOneCountry(@Arg("code", {defaultValue: "FR"}) code:string): Promise<Country | null> {
+      return await Country.findOne({
+         where: {
+            code
+         }
+      })
    }
 }
